5d67e5fd46a93e5b5ef51401a2f2bdcb6961074f
adith13 <adithnarein1305@gmail.com>
2023-11-21 12:51:11 +0530

    checking the git

--- teamProjectOne/src/main/java/com/adith/teamProjectOne/App.java
+++ teamProjectOne/src/main/java/com/adith/teamProjectOne/App.java
@@ -2,7 +2,7 @@ package com.adith.teamProjectOne;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello world forever!");
     }
    
 }
